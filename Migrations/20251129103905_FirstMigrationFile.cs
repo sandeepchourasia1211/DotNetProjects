73897a1f4bd52b84beb5fb8a52916e58ebafb43b
Sandeep Chourasia <schaurasiya1211@gmail.com>
2025-11-29 16:20:29 +0530

    [Sandeep - Basic Structure Created and DbContext Setup]

---
+++ Migrations/20251129103905_FirstMigrationFile.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFFirstApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigrationFile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Department",
+                columns: table => new
+                {
+                    Did = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Dname = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Department", x => x.Did);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    Eid = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Gender = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Salary = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DOB = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Did = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.Eid);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Department");
+
+            migrationBuilder.DropTable(
+                name: "Employee");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.Identity.Client;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+using var db = new OrganizationDb();
+
+// Ensures DB + Tables are created
+db.Database.EnsureCreated();
+
+Console.WriteLine("✔ Database & Tables Created Successfully!");
+
+OrganizationDb organizationDb=new OrganizationDb();
+List<Department> dept = organizationDb.Departments.ToList();
+
+foreach(var D in dept)
+{
+    Console.WriteLine($"Did : {D.Did}   DName: {D.Dname}   Description: {D.Description} ");
+}
+
+#region DepartmentTable/Design Entities
+[Table("Department")]
+public class Department
+{
+    [Key]
+    public int Did { get; set; }
+
+    public String? Dname { get; set; }
+    public String? Description { get; set; }
+}
+#endregion
+
+[Table("Employee")]
+public class Employee
+{
+    [Key]
+    public int Eid { get; set; }
+    public String? EName { get; set; }
+    public String? Gender { get; set; }
+    public String? Email { get; set; }
+    public String? Salary { get; set; }
+    public DateTime DOB {  get; set; }
+    public int? Did { get; set; }
+
+
+}
+#region EFCoreDbContext
+public class OrganizationDb : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseSqlServer("Server=localhost;Database=OrgEFDb;Trusted_Connection=True;TrustServerCertificate=True;");
+    }
+
+    public DbSet<Department> Departments { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+
+}
+#endregion
+
+
